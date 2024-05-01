dc159b52c7fba5e61801d625aabcee7be52eb20e
danielsm <danielsanchezm92@gmail.com>
2024-05-01 15:41:51 -0500

    Adding clan repository and fix @repository

--- ERP/src/main/java/NextBill/ERP/domain/repositories/ClanRepository.java
+++ ERP/src/main/java/NextBill/ERP/domain/repositories/ClanRepository.java
@@ -3,10 +3,9 @@ package NextBill.ERP.domain.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import NextBill.ERP.domain.entities.User;
+import NextBill.ERP.domain.entities.Clan;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Integer>{
+public interface ClanRepository extends JpaRepository<Clan, Integer>{
 
-     
 }
