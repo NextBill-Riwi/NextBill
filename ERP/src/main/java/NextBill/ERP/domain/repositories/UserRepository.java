0c4fcbed47354f9449959cae70fdda6ab340194e
danielsm <danielsanchezm92@gmail.com>
2024-05-01 15:27:36 -0500

    add repository user

---
+++ ERP/src/main/java/NextBill/ERP/domain/repositories/UserRepository.java
@@ -0,0 +1,10 @@
+package NextBill.ERP.domain.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import NextBill.ERP.domain.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Integer>{
+
+     
+}
