bef6dfdc6ccb21e8eee912b6d0202135a98680b0
dasanzmo <danielsanchezm92@gmail.com>
2024-05-05 17:00:35 -0500

    status addedo to module entity

--- ERP/src/main/java/NextBill/ERP/domain/entities/Module.java
+++ ERP/src/main/java/NextBill/ERP/domain/entities/Module.java
@@ -1,9 +1,11 @@
 package NextBill.ERP.domain.entities;
 
 import java.util.List;
-
+import NextBill.ERP.util.enums.ModuleStatus;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -24,6 +26,10 @@ public class Module {
     private int id;
     @Column(unique = true)
     private String name;
+
+    @Column(length = 45)
+    @Enumerated(EnumType.STRING)
+    private ModuleStatus status;
 
     @ToString.Exclude
     @EqualsAndHashCode.Exclude
